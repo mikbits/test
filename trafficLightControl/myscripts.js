b51cc39f40c4eefd2141b9c3648fac9b1cff551c
Imran Khan Mohammed <mik.bits@gmail.com>
2016-05-26 07:48:12 +1000

    tc3

--- trafficLightControl/myscripts.js
+++ trafficLightControl/myscripts.js
@@ -1,4 +1,4 @@
-(function() {
+
 	var TLM = {
 		init: function() {
 			var _this = this;
@@ -7,51 +7,41 @@
 		bindEvent: function(){
 			TLM.changeStateEW();
 			TLM.changeStateNS();
+			TLM.fun();
 		},
-		changeStateEW : (function(){
-				var stateEW = 0,
-				lampTime = 5000,
-				yellowLampTime = 3000,
-				lamps = ["Red", "Yellow", "Green"],
-				lampsLength = lamps.length,
-				orderEW = [
-					[lampTime, "Green"],
-					[yellowLampTime, "Yellow"],
-					[lampTime+yellowLampTime, "Red"]
-				],
-				orderLength = orderEW.length,
-				lampIndex,
-				orderIndex,
-				sIdEW;
-
-			return function () {
-				
-				setTimeout(function(){clearTimeout(sIdEW);return;},TLM.totalTiime);
-
-				var lamp,
-				lampDOM;
-
-				for (lampIndex = 0; lampIndex < lampsLength; lampIndex += 1) {
-					lamp = lamps[lampIndex];
-					lampDOME = document.getElementById(lamp+"East");
-					lampDOMW = document.getElementById(lamp+"West");
-					if (orderEW[stateEW].indexOf(lamp) !== -1) {
-						lampDOME.classList.add("lamp" + lamp);
-						lampDOMW.classList.add("lamp" + lamp);
-					} else {
-						lampDOME.classList.remove("lamp" + lamp);
-						lampDOMW.classList.remove("lamp" + lamp);
-					}
+		temp:0,
+		fun:function(){
+			TLM.temp+=1;
+			alert(TLM.temp);
+			setTimeout(TLM.fun,1000);
+		},
+		stateEW :0,
+		lampTime : 5000,
+		yellowLampTime : 3000,
+		sIdEW:null,		
+		changeStateEW : function(){				
+			setTimeout(function(){clearTimeout(TLM.sIdEW);return;},TLM.totalTiime);
+			var lamp,
+			lampDOM;
+			for (lampIndex = 0; lampIndex < TLM.lamps.length; lampIndex += 1) {
+				lamp = TLM.lamps[lampIndex];
+				lampDOME = document.getElementById(lamp+"East");
+				lampDOMW = document.getElementById(lamp+"West");
+				if (TLM.orderEW[TLM.stateEW].indexOf(lamp) !== -1) {
+					lampDOME.classList.add("lamp" + lamp);
+					lampDOMW.classList.add("lamp" + lamp);
+				} else {
+					lampDOME.classList.remove("lamp" + lamp);
+					lampDOMW.classList.remove("lamp" + lamp);
 				}
-				
-				sIdEW = setTimeout(TLM.changeStateEW, orderEW[stateEW][0]);
-				stateEW += 1;
-				if (stateEW >= orderLength) {
-					stateEW = 0;
-				}
-			};
-		})(),
-		
+			}
+			
+			TLM.sIdEW = setTimeout(TLM.changeStateEW, TLM.orderEW[TLM.stateEW][0]);
+			TLM.stateEW += 1;
+			if (TLM.stateEW >= TLM.lamps.length) {
+				TLM.stateEW = 0;
+			}
+	},
 		changeStateNS : (function () {
 				var stateNS = 0,
 				lampTime = 5000,
@@ -97,6 +87,10 @@
 		}()),
 		totalTiime:30000
 	};
-	
+	TLM.orderEW = [
+					[TLM.lampTime, "Green"],
+					[TLM.yellowLampTime, "Yellow"],
+					[TLM.lampTime+TLM.yellowLampTime, "Red"]
+				];
+	TLM.lamps = ["Red", "Yellow", "Green"];
 	TLM.init();
-})();
